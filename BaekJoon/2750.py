50194ceea6c0e301bea09977b4e9c648f8fb39d7
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-01-17 21:04:33 +0900

    Python 풀이

--- BaekJoon/2750.py
+++ BaekJoon/2750.py
@@ -1,1 +1,31 @@
-""" 문제 유형 : 정렬 """
+# 문제 유형 : 정렬
+
+n = int(input())
+array = list()
+
+for _ in range(n):
+    array.append(int(input()))
+
+for i in range(n):
+    # 가장 작은 원소의 번호
+    min_index = i
+    for j in range(i + 1, n):
+        if array[min_index] > array[j]:
+            min_index = j
+    # 순서 변경
+    array[i], array[min_index] = array[min_index], array[i]
+
+for i in array:
+    print(i)
+
+# 기본 정렬 라이브러리
+# n = int(input())
+# array = list()
+
+# for _ in range(n):
+#     array.append(int(input()))
+
+# array.sort()
+
+# for i in array:
+#     print(i)
