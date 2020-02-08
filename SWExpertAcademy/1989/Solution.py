f82ddb3ff82ef74be233f294e21dc1a73fe0a6f4
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-02-08 15:14:25 +0900

    안됨

--- SWExpertAcademy/1989/Solution.py
+++ SWExpertAcademy/1989/Solution.py
@@ -1,11 +1,36 @@
+def grade(idx):
+    score = {
+        0: 'D0',
+        1: 'C-',
+        2: 'C0',
+        3: 'C+',
+        4: 'B-',
+        5: 'B0',
+        6: 'B+',
+        7: 'A-',
+        8: 'A0',
+        9: 'A+'
+    }
+    return score.get(idx, '{} is invalid index (0-9)'.format(idx))
+
+
 T = int(input())
 
-for test_case in range(1, T + 1):
-    text = str(input())
-    length = len(text)
-    result = 1
-    for i in range(length):
-        if(text[i] != text[-i-1]):
-            result = 0
+for test_case in (1, T+1):
+    n, k = map(int, input().split())
+    arr = []
+    for i in range(1, n+1):
+        li = list(map(int, input().split()))
+        sum = li[0]*0.35 + li[1]*0.45 + li[2]*0.2
+        if len(arr) != 0:
+            for j in range(len(arr), -1, -1):
+                if(arr[j-1][1] < sum or j == 0):
+                    arr.insert(j, [i, sum])
+                    break
+        else:
+            arr.append([i, sum])
+
+    for i in range(len(arr)):
+        if(arr[i][0] == k):
+            print('#{} {}'.format(test_case, grade(i)))
             break
-    print('#{} {}'.format(test_case, result))
