a8eccc869dfd259df898df1c46c973e5b06c3ec5
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-02-15 23:49:28 +0900

    1945

--- SWExpertAcademy/1970/Solution.py
+++ SWExpertAcademy/1970/Solution.py
@@ -1,22 +1,46 @@
 T = int(input())
 
 for test_case in range(1, T+1):
-    n = int(input())
-    arr = []
-    m = 50000
-    switch = 0
+    num = int(input())
+
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+
     while True:
-        if(m < 10):
-            break
-        arr.append(int(n / m))
-        n = n % m
-        if(switch == 0):
-            m = m/5
-            switch = 1
+        if(num % 2 == 0):
+            num = num/2
+            a += 1
         else:
-            m = m/2
-            switch = 0
-    print('#{}'.format(test_case))
-    for i in range(len(arr)):
-        print(arr[i], end=' ')
-    print(' ')
+            break
+
+    while True:
+        if(num % 3 == 0):
+            num = num/3
+            b += 1
+        else:
+            break
+
+    while True:
+        if(num % 5 == 0):
+            num = num/5
+            c += 1
+        else:
+            break
+
+    while True:
+        if(num % 7 == 0):
+            num = num/7
+            d += 1
+        else:
+            break
+
+    while True:
+        if(num % 11 == 0):
+            num = num/11
+            e += 1
+        else:
+            break
+    print('#{} {} {} {} {} {}'.format(test_case, a, b, c, d, e))
