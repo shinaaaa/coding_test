68b45640ebe8c55e65e903720f6be921f94356a6
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-02-17 23:41:21 +0900

    어려움

--- SWExpertAcademy/2001/Solution.py
+++ SWExpertAcademy/2001/Solution.py
@@ -8,7 +8,4 @@ for test_case in range(1, T+1):
         li = list(map(int, input().split()))
         arr.append(li)
 
-    for i in range(n):
-        for j in range(n):
-            print('i')
     print(arr)
