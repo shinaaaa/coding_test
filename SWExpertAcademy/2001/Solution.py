8a3e2fedc37cd1c4a3b4092da2a6864be0bbcf71
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-02-18 12:55:53 +0900

    2001

--- SWExpertAcademy/2001/Solution.py
+++ SWExpertAcademy/2001/Solution.py
@@ -5,7 +5,31 @@ for test_case in range(1, T+1):
     arr = []
 
     for i in range(n):
-        li = list(map(int, input().split()))
-        arr.append(li)
+        arr.append(list(map(int, input().split())))
 
-    print(arr)
+    x = 0
+    y = 0
+    switch = 0
+    MAX = 0
+
+    while True:
+        value = 0
+        if(switch == 1):
+            y += 1
+            switch = 0
+            if(y == n-m+1):
+                break
+
+        for i in range(y, m+y):
+            for j in range(x, m+x):
+                value += arr[i][j]
+
+        if(MAX < value):
+            MAX = value
+
+        if(switch == 0):
+            x += 1
+            if(x == n-m+1):
+                x = 0
+                switch = 1
+    print('#{} {}'.format(test_case, MAX))
