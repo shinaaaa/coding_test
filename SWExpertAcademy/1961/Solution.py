1f4d8bc262743989b38e57fc91f80c1194ecb92b
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-02-19 22:17:44 +0900

    1961

---
+++ SWExpertAcademy/1961/Solution.py
@@ -0,0 +1,29 @@
+import copy
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    n = int(input())
+    arr = []
+    count = 0
+    for i in range(n):
+        arr.append(list(map(int, input().split())))
+
+    il = []
+    result = []
+
+    while True:
+        for i in range(n):
+            text = ""
+            a = []
+            for j in range(n-1, -1, -1):
+                text += str(arr[j][i])
+                a.append(arr[j][i])
+            result.append(text)
+            il.append(a)
+            il.clear()
+        count += 1
+        if count > 2:
+            break
+    print(result)
+    print(il)
