53d67ed1da460a67a933389407a6f2d313e84103
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-03-02 23:32:44 +0900

    1959 미완성

---
+++ SWExpertAcademy/1959/Solution.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+
+for test_case in range(1, T + 1):
+    n, m = map(int, input().split())
+
+    print('n : ', n)
+    print('m : ', m)
+
+    arr1 = list(map(int, input().split()))
+
+    arr2 = list(map(int, input().split()))
+
+    print('arr1 : ', arr1)
+    print('arr2 : ', arr2)
