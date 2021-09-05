3dce5a43293f10a99406e32b6c258aa60da40a27
shinaaaa <shin029abc@gmail.com>
2021-09-05 17:40:20 +0900

    숫자 문자열과 영단어

--- Programmers/81301/solution.py
+++ Programmers/81301/solution.py
@@ -1,13 +1,8 @@
 def solution(s):
-    s = s.replace("zero", "0")
-    s = s.replace("one", "1")
-    s = s.replace("two", "2")
-    s = s.replace("three", "3")
-    s = s.replace("four", "4")
-    s = s.replace("five", "5")
-    s = s.replace("six", "6")
-    s = s.replace("seven", "7")
-    s = s.replace("eight", "8")
-    s = s.replace("nine", "9")
+    dic = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7",
+           "eight": "8", "nine": "9"}
+
+    for k, v in dic.items():
+        s = s.replace(k, v)
 
     return int(s)
